fdb79b86f239bde444604d58005136f18dceb0a6
raneen <raneen@LAPTOP-S7U5TJ2U>
2026-05-04 11:40:36 +0300

    Add project files.

---
+++ TaskApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TaskApp/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace TaskApp
+{
+    using System.Collections.ObjectModel;
+    using System.Diagnostics;
+    using System.Formats.Tar;
+
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<TaskItem> Tasks { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Tasks = new ObservableCollection<TaskItem>();
+            taskList.ItemsSource = Tasks;
+        }
+
+        private void OnAddTaskClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(taskEntry.Text))
+            {
+                Tasks.Add(new TaskItem { Name = taskEntry.Text });
+                taskEntry.Text = "";
+            }
+        }
+
+        private void OnDeleteClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            var task = button.BindingContext as TaskItem;
+            Tasks.Remove(task);
+        }
+    }
+
+    public class TaskItem
+    {
+        public string Name { get; set; }
+    }
+}ssss
